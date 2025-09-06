c25d1f0b08c8330f7d855d0c3526fbae89ebc744
shreekutta0311-dotcom <shreekutta0311@gmail.com>
2025-09-06 22:18:10 +0530

    Create q1.cpp

---
+++ set3/q1.cpp
@@ -0,0 +1,10 @@
+/*1. Write a program to print numbers from 1 to 10 using a for loop.*/
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <11; i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
