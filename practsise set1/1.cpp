699067a7d0c2693d91a775a27d9d68476388e6c4
shreekutta0311-dotcom <shreekutta0311@gmail.com>
2025-09-06 22:13:32 +0530

    Create 1.cpp

---
+++ practsise set1/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int age;
+string name;
+cout<<"type ur name:"<<endl;
+cin>>name;
+cout<<"type ur age:"<<endl;
+cin>>age;
+cout<<"name is: " <<name<< " age is:"<<age<<endl;
+
+
+    return 0;
+}
